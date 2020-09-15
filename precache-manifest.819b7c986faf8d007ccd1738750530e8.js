beedcd94448ec4429b17f94e4166e75380b42cae
Max <maksimavramenko33@gmail.com>
2020-09-15 12:44:34 +0300

    Updates

---
+++ precache-manifest.819b7c986faf8d007ccd1738750530e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6beec0f1858f189eaaff53b919cf0e",
+    "url": "/HW3-react/index.html"
+  },
+  {
+    "revision": "28463edaebbef505388a",
+    "url": "/HW3-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c6257935719475056f27",
+    "url": "/HW3-react/static/js/2.b8316cae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW3-react/static/js/2.b8316cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28463edaebbef505388a",
+    "url": "/HW3-react/static/js/main.eb3056f6.chunk.js"
+  },
+  {
+    "revision": "ae07222e38cacddd2810",
+    "url": "/HW3-react/static/js/runtime-main.e6798ed9.js"
+  }
+]);
